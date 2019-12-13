163590d6411b4d2ae3697751e6a98e53529ddd41
Görkem Güler <57195990+mrdauble@users.noreply.github.com>
2019-12-13 19:20:38 +0300

    Update yemekhaneselcuk.py

--- yemekhaneselcuk.py
+++ yemekhaneselcuk.py
@@ -1,19 +1,15 @@
 from bs4 import BeautifulSoup
-
 import webbrowser
-import urllib.request # python3x request kütüphanesi
-#url belirtelim.
-url = "https://www.selcuk.edu.tr/saglik_kultur/birim/web/sayfa/ayrinti/1473/tr"
-#url'i okuyalum.
-url_oku = urllib.request.urlopen(url)
-#BeautifulSoup yollyalım.
-soup = BeautifulSoup(url_oku, 'html.parser')
-# tüm html dökümanı ekrana basalım
-	
-ana = soup.find('span',attrs={"id": "ContentPlaceHolder1_lblIcerik"})
+import urllib.request 
 
-resim=ana.find('img')
-resimac=resim['src']
+url = "https://www.selcuk.edu.tr/saglik_kultur/birim/web/sayfa/ayrinti/1473/tr"
+url_oku = urllib.request.urlopen(url)
+
+soup = BeautifulSoup(url_oku, 'html.parser')
+
+ana = soup.find('span',attrs={"id": "ContentPlaceHolder1_lblIcerik"}) #span etiketinde aradağımız id değerini bulalım
+resim=ana.find('img') # resimleri bulalım
+resimac=resim['src'] 
 
 
 webbrowser.open_new(resimac)
